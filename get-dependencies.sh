13e816ecaa9476ec2614bab6f890b4a5ec457886
Link4Electronics <link4electronics@gmail.com>
2026-04-08 06:30:43 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,25 +8,45 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     glu        \
-    libdecor   \
+    libgme     \
     libopenmpt \
-    sdl2_mixer \
+    miniupnpc  \
+    sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-sed -i -e 's|-O2|-O2 -std=gnu11|' /etc/makepkg.conf
-make-aur-package srb2-data
-make-aur-package srb2
+echo "Building stable version of Sonic Robo Blast 2..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/STJr/SRB2"
+RELEASE="$(curl -fsSL https://api.github.com/repos/STJr/SRB2/releases/latest)"
+TAG="$(echo "$RELEASE" | grep '"tag_name"' | head -1 | cut -d '"' -f 4)"
+FULL_ZIP_URL="$(echo "$RELEASE" | grep '"browser_download_url"' | grep -- '-Full\.zip' | head -1 | cut -d '"' -f 4)"
+git clone --branch "$TAG" --depth 1 "$REPO" ./SRB2
+VERSION="${TAG#SRB2_release_}"
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+curl -fL -O "$FULL_ZIP_URL"
+FULL_ZIP="$(basename "$FULL_ZIP_URL")"
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+mkdir -p ./AppDir/bin
+mkdir -p ./AppDir/share/games/SRB2
+bsdtar -xf "$FULL_ZIP" -C ./AppDir/share/games/SRB2 \
+    models.dat music.pk3 srb2.pk3 zones.pk3 characters.pk3
+if bsdtar -xf "$FULL_ZIP" -C ./AppDir/share/games/SRB2 patch.pk3 2>/dev/null; then
+    echo "patch.pk3 found and installed"
+else
+    echo "patch.pk3 not found, continuing without it"
+fi
+rm -f "$FULL_ZIP"
+
+cd ./SRB2/src
+# make comptime.sh optional
+sed 's/^comptime\.c ::/comptime.c :/' -i Makefile
+# use better version string
+sed 's/-DCOMPVERSION//' -i Makefile
+sed 's/illegal/AUR/' -i comptime.c
+
+make LINUX64=1 NOUPX=1 NOVERSION=1 CFLAGS="-std=gnu11" -j$(nproc)
+mv -v ../bin/lsdl2srb2 ../../AppDir/bin/srb2
