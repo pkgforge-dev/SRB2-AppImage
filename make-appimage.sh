e935fc456d2463c69e64726e8c301c4f0bcaee8f
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:41:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q srb2 | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://git.do.srb2.org/STJr/SRB2/-/raw/next/srb2.png?ref_type=heads
 export DESKTOP=/usr/share/applications/srb2-opengl.desktop
