17ba01cb65919293257a655d93f2e40d6ff82fc4
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:41:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,24 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q srb2 | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=https://git.do.srb2.org/STJr/SRB2/-/raw/next/srb2.png?ref_type=heads
-export DESKTOP=/usr/share/applications/srb2-opengl.desktop
-export APPNAME="Sonic Robo Blast 2"
-export STARTUPWMCLASS=srb2
+export ICON=https://raw.githubusercontent.com/STJr/SRB2/refs/heads/master/srb2.png
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/srb2
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/srb2
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# test the final app
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
 quick-sharun --simple-test ./dist/*.AppImage
