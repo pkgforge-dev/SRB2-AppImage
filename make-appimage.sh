c55279398ed3e95354980273d1b5521a8a46619f
Link4Electronics <link4electronics@gmail.com>
2026-09-01 12:52:13 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,15 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q srb2 | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/STJr/SRB2/refs/heads/master/srb2.png
-export DESKTOP=/usr/share/applications/srb2-opengl.desktop
-export APPNAME="Sonic Robo Blast 2"
-export STARTUPWMCLASS=srb2
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/srb2
+quick-sharun ./AppDir/bin/srb2
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
